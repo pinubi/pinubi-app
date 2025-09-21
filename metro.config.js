28bcf912d67d8284f2e677164060a16edfbc66ff
Allan Winckler <awmoreira@gmail.com>
2025-09-21 01:26:50 -0300

    Metro config update

--- metro.config.js
+++ metro.config.js
@@ -1,8 +1,12 @@
-const { getDefaultConfig } = require("expo/metro-config");
+const { getDefaultConfig } = require('expo/metro-config');
 const { withNativeWind } = require('nativewind/metro');
- 
-const config = getDefaultConfig(__dirname)
-config.resolver.sourceExts.push('cjs');
+
+const config = getDefaultConfig(__dirname);
+config.resolver.sourceExts = config.resolver.sourceExts || [];
+if (!config.resolver.sourceExts.includes('cjs')) {
+  config.resolver.sourceExts.push('cjs');
+}
+
 config.resolver.unstable_enablePackageExports = false;
- 
-module.exports = withNativeWind(config, { input: './src/global.css' })
+
+module.exports = withNativeWind(config, { input: './src/global.css' });
